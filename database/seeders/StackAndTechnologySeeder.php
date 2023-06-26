2e9c47fa7e2d3b52bb296e3429c541499ccf4491
diana.pascari <diana.pascari@mail.ebs-integrator.com>
2023-06-26 17:20:10 +0300

    add_seeder_for_stacks_and_technologies

--- database/seeders/StackAndTechnologySeeder.php
+++ database/seeders/StackAndTechnologySeeder.php
@@ -48,7 +48,7 @@ class StackAndTechnologySeeder extends Seeder
         ];
 
         foreach ($stackTechnologies as $stackName => $technologies) {
-            $stack = Stack::where('name', $stackName)->firstOrFail();
+            $stack = Stack::where('name', $stackName)->firstOrCreate();
 
             foreach ($technologies as $technologyName) {
                 Technology::updateOrCreate([
