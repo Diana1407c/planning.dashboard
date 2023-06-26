213ed9c2ae2c2a7573adbe70956b16c3fd55da2d
diana.pascari <diana.pascari@mail.ebs-integrator.com>
2023-06-26 17:14:21 +0300

    add_seeder_for_stacks_and_technologies

---
+++ database/seeders/StackAndTechnologySeeder.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Stack;
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StackAndTechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $stacks = [
+            'Backend',
+            'Frontend',
+            'Mobile',
+            'Analysis',
+        ];
+
+        foreach ($stacks as $stackName) {
+            Stack::updateOrCreate(['name' => $stackName]);
+        }
+        $stackTechnologies = [
+            'Backend' => [
+                'Python',
+                'PHP',
+                'Java',
+                '.NET',
+            ],
+            'Frontend' => [
+                'React',
+            ],
+            'Mobile' => [
+                'iOS',
+                'Flutter',
+                'Android',
+            ],
+            'Analysis' => [
+                'BA',
+                'DevOps',
+                'Design',
+                'QA',
+            ],
+        ];
+
+        foreach ($stackTechnologies as $stackName => $technologies) {
+            $stack = Stack::where('name', $stackName)->firstOrFail();
+
+            foreach ($technologies as $technologyName) {
+                Technology::updateOrCreate([
+                    'name' => $technologyName,
+                    'stack_id' => $stack->id,
+                ]);
+            }
+        }
+    }
+}
