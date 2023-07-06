ad8773d3cb4b598197b27ad71fe95b39bdfbd2c3
gzadic <gheorghe.zadic@ebs-inetgrator.com>
2023-07-06 10:40:46 +0300

    fix schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\Team;
 use App\Observers\TeamObserver;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('APP_ENV') !== 'dev') {
+            $this->app['request']->server->set('HTTPS', 'on');
+            URL::forceScheme('https'); // Force HTTPS
+        }
     }
 }
