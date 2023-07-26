73278b71f826f83694257398f416ccfe459deaf4
diana.pascari <diana.pascari@mail.ebs-integrator.com>
2023-07-26 15:01:39 +0300

    add_seeder_for_holidays

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,5 +22,6 @@ class DatabaseSeeder extends Seeder
         $this->call([RolesAndPermissionsSeeder::class]);
         $this->call([StackAndTechnologySeeder::class]);
         $this->call([AdminSeeder::class]);
+        $this->call([HolidaysSeeder::class]);
     }
 }
