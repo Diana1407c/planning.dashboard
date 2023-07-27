713db60e4d77f939acc9911979ac46a7f2f0d33d
diana.pascari <diana.pascari@mail.ebs-integrator.com>
2023-07-27 11:32:04 +0300

    add_seeder_for_holidays

--- database/seeders/HolidaysSeeder.php
+++ database/seeders/HolidaysSeeder.php
@@ -16,6 +16,28 @@ class HolidaysSeeder extends Seeder
         $currentYear = date('Y');
         $endYear = 2099;
 
+        $fixedholidays = [
+            ['01-01', "New Year's Day"],
+            ['01-07', "Orthodox Christmas"],
+            ['01-08', "Orthodox Christmas Day 2"],
+            ['03-08', "International Women's Day"],
+            ['05-01', "Labour Day"],
+            ['05-09', "Europe Day and Victory Day"],
+            ['08-27', "Independence Holiday"],
+            ['08-31', "National Language Day (Moldova)"],
+            ['10-14', "Chisinau City Day"],
+        ];
+
+        foreach ($fixedholidays as $holidayData) {
+            $date = $currentYear. '-' . $holidayData[0];
+            $name = $holidayData[1];
+
+            Holiday::updateOrCreate(
+                ['date' => $date, 'name' => $name],
+                ['every_year' => 1]
+            );
+        }
+
         for ($year = $currentYear; $year <= $endYear; $year++) {
 
             $easterTimestamp = orthodox_eastern($year);
@@ -23,43 +45,19 @@ class HolidaysSeeder extends Seeder
             $memorialTimestamp = memorial_orthodox_eastern($year);
             $memorialEasterDate = date('m-d', $memorialTimestamp);
 
-            $holidays = [
-                ['01-01', "New Year's Day"],
-                ['01-07', "Orthodox Christmas"],
-                ['01-08', "Orthodox Christmas Day 2"],
-                ['03-08', "International Women's Day"],
-                ['05-01', "Labour Day"],
-                ['05-09', "Europe Day and Victory Day"],
-                ['08-27', "Independence Holiday"],
-                ['08-31', "National Language Day (Moldova)"],
-                ['10-14', "Chisinau City Day"],
+            $dynamicHolidays = [
                 [$easterDate, "Orthodox Easter"],
                 [$memorialEasterDate, "Memorial Easter"],
             ];
 
-            foreach ($holidays as $holidayData) {
+            foreach ($dynamicHolidays as $holidayData) {
                 $date = $year . '-' . $holidayData[0];
                 $name = $holidayData[1];
 
-                if (($name === "Labour Day") &&
-                    ($date === $year . '-' . $easterDate || $date === $year . '-' . $memorialEasterDate)) {
-                    Holiday::updateOrCreate(
-                        ['date' => $date, 'name' => 'Labour Day'],
-                        ['every_year' => 1]
-                    );
-                }
-                elseif (($name === "Europe Day and Victory Day") &&
-                    ($date === $year . '-' . $easterDate || $date === $year . '-' . $memorialEasterDate)) {
-                    Holiday::updateOrCreate(
-                        ['date' => $date, 'name' => 'Europe Day and Victory Day'],
-                        ['every_year' => 1]
-                    );
-                }else {
-                    Holiday::updateOrCreate(
-                        ['date' => $date, 'name' => $name],
-                        ['every_year' => 1]
-                    );
-                }
+                Holiday::updateOrCreate(
+                    ['date' => $date, 'name' => $name],
+                    ['every_year' => 0]
+                );
             }
         }
     }
