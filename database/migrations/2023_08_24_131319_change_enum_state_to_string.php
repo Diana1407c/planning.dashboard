6f786fd50ffd565fe1aed5dc228c4c3ee99deb84
diana.pascari <diana.pascari@mail.ebs-integrator.com>
2023-08-24 17:24:17 +0300

    update sync projects

---
+++ database/migrations/2023_08_24_131319_change_enum_state_to_string.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Project;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('state')->nullable()->default(Project::STATE_ACTIVE)->after('name')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('state')->nullable()->default(Project::STATE_ACTIVE)->after('name')->change();
+        });
+    }
+};
